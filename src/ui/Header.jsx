d35e554276125c430a4475c51bc24f81fd5a2d98
rabobahago <rabobahago@gmail.com>
2023-07-04 17:33:23 +0100

    router create order actions added

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,8 +1,10 @@
 import { Link } from "react-router-dom";
+import SearchOrder from "../features/order/SearchOrder";
 export default function Header() {
   return (
     <header>
       <Link to="/">Fast React Pizza Co.</Link>
+      <SearchOrder />
       <p>Rabo</p>
     </header>
   );
