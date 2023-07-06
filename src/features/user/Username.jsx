a9eadf3e8cdbd7fdce97c2a3c44a9262c17fc2e4
rabobahago <rabobahago@gmail.com>
2023-07-06 16:57:47 +0100

    css tailwind grid and flexbox layout added

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
 
 export default function Username() {
-  return <div className="hidden text-sm font-semibold">Rabo</div>;
+  return <div className="hidden text-sm font-semibold md:block">Rabo</div>;
 }
