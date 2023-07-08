b80733880cbf283792b586b38b21376c7afd8938
rabobahago <rabobahago@gmail.com>
2023-07-08 15:21:07 +0100

    cart page styled with tailwind css

--- src/utils/Button.jsx
+++ src/utils/Button.jsx
@@ -1,16 +1,23 @@
 import { Link } from 'react-router-dom';
 
-export default function Button({ children, disabled, to }) {
-  let className =
-    'inline-block rounded-full bg-yellow-400 px-4 py-3 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 disabled:cursor-not-allowed sm:px-6 sm:py-4';
-  if (to)
+export default function Button({ children, disabled, to, type }) {
+  let base =
+    'inline-block rounded-full bg-yellow-400 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 disabled:cursor-not-allowed';
+  let styles = {
+    primary: base + ' px-4 py-3 md:px-6 md:py-4',
+    small: base + ' px-4 py-2 md:px-5 md:py-2.5 text-xs',
+    secondary:
+      'inline-block rounded-full border-2 border-stone-300 font-semibold uppercase tracking-wide text-stone-400 transition-colors duration-300 hover:bg-stone-300 focus:bg-stone-300 hover:text-stone-800 focus:outline-none focus:ring focus:text-stone-800 focus:ring-stone-200 disabled:cursor-not-allowed px-4 py-2.5 md:px-6 md:py-3.5',
+  };
+  if (to === '-1') {
     return (
-      <Link to={to} className={className}>
+      <Link to={to} className={styles[type]}>
         {children}
       </Link>
     );
+  }
   return (
-    <button disabled={disabled} className={className}>
+    <button disabled={disabled} className={styles[type]}>
       {children}
     </button>
   );
